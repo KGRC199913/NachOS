b3b205b46365880448c8827cda1205728a6fcaad
Tuan Pham <phamtuan1999@gmail.com>
2019-04-14 06:09:08 -0700

    fix append

--- nachos-3.4/code/test/append.c
+++ nachos-3.4/code/test/append.c
@@ -2,36 +2,59 @@
 
 int main() {
 	char buffer[100];
-	OpenFileId srcId, desId;
-	int r, test, len;
-	Write("Src: \n", 6, 1);
-	Read(buffer, 100, 0);
-	srcId = Open(buffer, 0);
-	if (srcId < 1) {
-		CloseF(srcId);
-		Write("Error: Source file not found\n", 30, 1);
-		Halt(); 
-	} 
-	
-	Write("Des: \n", 6, 1);
+	OpenFileId fstId, secId, desId;
+	int r, w;
 	r = Read(buffer, 100, 0);
-	buffer[r] = '\0';
-	desId = Open(buffer, 0);
-	if (desId < 0) {
-		CreateFile(buffer);
-		desId = Open(buffer, 0);
+	buffer[r] = 0;
+	fstId = Open(buffer, 0);
+	if (fstId < 2) {
+		Write("Error orcurred when open first file\n", 37, 1);
+		Halt();
 	}
-	Seek(-1, desId);
+	
+	r = Read(buffer, 100, 0);
+	buffer[r] = 0;
+	secId = Open(buffer, 0);	
+	if (secId < 2) {
+		CloseF(fstId);
+		Write("Error orcurred when open second file\n", 38, 1);
+		Halt();
+	}
+
+	r = Read(buffer, 100, 0);
+	buffer[r] = 0;
+	desId = Open(buffer, 0);
+	if (desId > 0) {
+		CloseF(fstId);
+		CloseF(secId);
+		Write("Destination file is already existed\n", 37, 1);
+		Halt();
+	}
+	CreateFile(buffer);
+	desId = Open(buffer, 0);
+	if (desId < 2) {
+		CloseF(fstId);
+		CloseF(secId);
+		Write("Error orcurred when open third file\n", 38, 1);
+		Halt();
+	}
+	
 	do {
-		r = Read(buffer, 100, srcId);
-		if (r <= 0) {
+		r = Read(buffer, 100, fstId);
+		if (r <= 0)
 			break;
-		}
-		Write(buffer, 100, desId);
-	} while (r > 0);
+		Write(buffer, r, desId);
+	} while (1);
+	CloseF(fstId);	
 
-	CloseF(srcId);
+	do {
+		r = Read(buffer, 100, secId);
+		if (r <= 0)
+			break;
+		Write(buffer, r, desId);
+	} while (1);
+	CloseF(secId);
+
 	CloseF(desId);
-
 	Halt();
 }
